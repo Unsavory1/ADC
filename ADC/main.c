ccf0bfb78c192c55fb94feea5f5570ca5ce77c95
Unsavory1 <sameer2120041@outlook.com>
2024-07-10 12:57:36 +0530

    adding delay config

--- ADC/main.c
+++ ADC/main.c
@@ -1,10 +1,15 @@
 #include "stm32f10x.h"
 
 #define Ctrl_Countflag (1 << 16)
+#define EOC_flag 0x02
+
+int val = 0;
 
 void ADC_Config(void);
 void GPIO_Config(void);
 void delay(int ms);
+void ADC1_2_IRQHandler(void);
+
 int main() {}
 
 void delay(int ms) {
@@ -22,7 +27,7 @@ void delay(int ms) {
 void GPIO_Config(void) {
   RCC->APB2ENR |= (1 << 2); // enables pin A
   RCC->APB2ENR |= (1 << 0); // enables alternate io
-  GPIOA->CRL |= 0;
+  GPIOA->CRL |= (1 <<);
 }
 
 void ADC_Config(void) {
@@ -31,13 +36,18 @@ void ADC_Config(void) {
   RCC->APB2ENR |= (1 << 9);
   ADC1->CR1 |= (1 << 5);       // enables end of conversion interupt
   NVIC_EnableIRQ(ADC1_2_IRQn); // interrupt handler
-
   // setting samping rate
   ADC1->SMPR2 |= (1 << 17) | (1 << 16) | (1 << 15);
   ADC1->SQR3 |= (5 << 0); // setting channel
   ADC1->CR2 |=
       (1 << 0) | (1 << 1); // enabling adc and setting it to continous mode
   delay(1);
-  ADC1->CR2 |= (1 << 2);
+  ADC1->CR2 |= (1 << 2); // calibration
   delay(2);
+}
+
+void ADC1_2_IRQHandler(void) {
+  if (ADC1->SR & EOC_flag) {
+    val = ADC1->DR; // clearing the eoc flag by reading the data register
+  }
 }
